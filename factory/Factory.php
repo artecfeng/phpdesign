f489b3df5cae1e7b89ceb8a07df3e8b83fdaa3ee
冯腾举 <824431999@qq.com>
2018-06-27 17:41:15 +0800

    添加单例模式

--- factory/Factory.php
+++ factory/Factory.php
@@ -5,41 +5,104 @@
      * Date: 2018/6/27
      * Time: 下午5:15
      */
+
     //数据库驱动类方法接口
-    interface DbInterface{
+    interface DbInterface {
         //数据库连接
         public function conn();
+
+        public static function getInstance();
     }
+
     //mysql驱动继承db接口
-    class MysqlDriver implements DbInterface{
+    class MysqlDriver implements DbInterface {
+        protected static $db;
+
+        private function __construct() {
+            //防止new
+        }
+
+        private function __clone() {
+            // TODO: Implement __clone() method.
+            //防止克隆
+        }
+
+        //单例模式
+        public static function getInstance() {
+            if (!self::$db) {
+                self::$db = new self();
+            }
+            return self::$db;
+        }
+
         public function conn() {
             // TODO: Implement conn() method.
-            return 'mysql conn'.PHP_EOL;
+            return 'mysql conn' . PHP_EOL;
         }
     }
+
     //oracle驱动继承db接口
-    class OracleDriver implements DbInterface{
+    class OracleDriver implements DbInterface {
+
+        protected static $db;
+
+        private function __construct() {
+            //防止new
+        }
+
+        private function __clone() {
+            // TODO: Implement __clone() method.
+            //防止克隆
+        }
+
+        //单例模式
+        public static function getInstance() {
+            if (!self::$db) {
+                self::$db = new self();
+            }
+            return self::$db;
+        }
+
         public function conn() {
             // TODO: Implement conn() method.
-            return 'oracle conn '.PHP_EOL;
+            return 'oracle conn ' . PHP_EOL;
         }
     }
 
     //数据库驱动工厂接口
-    interface FactoryInterface{
+    interface FactoryInterface {
         public static function newDb();
     }
 
-    class MysqlFactory implements FactoryInterface{
+    class MysqlFactory implements FactoryInterface {
+        protected static $db;
+
+        private function __construct() {
+            //防止new
+        }
+
+        private function __clone() {
+            // TODO: Implement __clone() method.
+            //防止克隆
+        }
+
+        //单例模式
+        public static function getInstance() {
+            if (!self::$db) {
+                self::$db = new self();
+            }
+            return self::$db;
+        }
+
         public static function newDb() {
             // TODO: Implement newDb() method.
-            return new MysqlDriver();
+            return MysqlDriver::getInstance();
         }
     }
 
-    class OracleFactory implements FactoryInterface{
+    class OracleFactory implements FactoryInterface {
         public static function newDb() {
             // TODO: Implement newDb() method.
-            return new OracleDriver();
+            return OracleDriver::getInstance();
         }
     }
